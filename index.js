3dfdb72c024e5e12ef79e0b518a4dd3c6720cf7c
vikashpc02 <114812878+vikashpc02@users.noreply.github.com>
2024-02-01 22:56:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('Project #13 - Deploy NodeJS app on AWS EC2 instance'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
